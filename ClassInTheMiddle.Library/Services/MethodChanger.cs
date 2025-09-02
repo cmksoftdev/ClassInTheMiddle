aaad836e5f3b925f45eaa1379399767de1920a21
cmk1988 <cmk88@gmx.de>
2025-09-02 23:33:36 +0200

    hooks working well now

--- ClassInTheMiddle.Library/Services/MethodChanger.cs
+++ ClassInTheMiddle.Library/Services/MethodChanger.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace ClassInTheMiddle.Library.Services
 {
     public class MethodChanger
     {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+        const uint PAGE_EXECUTE_READWRITE = 0x40;
+
+
         public static void Change(ConstructorInfo methodToReplace, ConstructorInfo methodToInject)
         {
 
@@ -92,69 +99,64 @@ namespace ClassInTheMiddle.Library.Services
 
         public static void Change(MethodInfo methodToReplace, MethodInfo methodToInject)
         {
-
-            //Thread.Sleep(1000);
+            RuntimeHelpers.PrepareMethod(methodToReplace.MethodHandle);
+            RuntimeHelpers.PrepareMethod(methodToInject.MethodHandle);
 
             unsafe
             {
+                // 32-bit vs 64-bit Pointergröße
+                int ptrSize = IntPtr.Size;
+                byte* tarPtr;
+                byte* injPtr;
 
-
-                if (IntPtr.Size == 4)
+                if (ptrSize == 8) // x64
                 {
-                    int* init_inj = (int*)methodToInject.MethodHandle.Value.ToPointer() + 2;
-                    int* init_tar = (int*)methodToReplace.MethodHandle.Value.ToPointer() + 2;
+                    // Offsets innerhalb MethodDesc x64 (nur m_pCodeOrIL)
+                    const int offset = 8 + 2 + 4;// + 2 + 4 + 8; // m_pMethodTable+ m_wFlags+padding+ m_slot+ m_pILHeader
 
-                    RuntimeHelpers.PrepareMethod(methodToReplace.MethodHandle);
-                    RuntimeHelpers.PrepareMethod(methodToInject.MethodHandle);
+                    tarPtr = (byte*)methodToReplace.MethodHandle.Value.ToPointer() + offset;
+                    injPtr = (byte*)methodToInject.MethodHandle.Value.ToPointer() + offset;
 
-                    int* inj = (int*)methodToInject.MethodHandle.Value.ToPointer() + 2;
-                    int* tar = (int*)methodToReplace.MethodHandle.Value.ToPointer() + 2;
-
-                    do
-                    {
-                        inj = (int*)methodToInject.MethodHandle.Value.ToPointer() + 2;
-                        tar = (int*)methodToReplace.MethodHandle.Value.ToPointer() + 2;
-                    }
-                    while (init_inj == inj || init_tar == tar);
-
-                    *tar = *inj;
+                    *(ulong*)tarPtr = *(ulong*)injPtr; // nur 8 Byte schreiben
                 }
-                else
+                else // x86
                 {
-                    //if ((long*)methodToInject.MethodHandle.Value.ToPointer() == (long*)0 || (long*)methodToReplace.MethodHandle.Value.ToPointer() == (long*)0)
-                    //{
-                    //}
-                    //long* inj = (long*)methodToInject.MethodHandle.Value.ToPointer() + 1;
-                    //long* tar = (long*)methodToReplace.MethodHandle.Value.ToPointer() + 1;
+                    // Offsets innerhalb MethodDesc x86 (nur m_pCodeOrIL)
+                    const int offset = 4 + 2 + 2 + 4 + 4; // m_pMethodTable + m_wFlags + padding + m_slot + m_pILHeader
 
+                    tarPtr = (byte*)methodToReplace.MethodHandle.Value.ToPointer() + offset;
+                    injPtr = (byte*)methodToInject.MethodHandle.Value.ToPointer() + offset;
 
-
-                    long init_inj = (long)methodToInject.MethodHandle.GetFunctionPointer().ToPointer();
-                    long init_tar = (long)methodToReplace.MethodHandle.GetFunctionPointer().ToPointer();
-
-                    RuntimeHelpers.PrepareMethod(methodToReplace.MethodHandle);
-                    RuntimeHelpers.PrepareMethod(methodToInject.MethodHandle);
-
-                    long _inj = (long)methodToInject.MethodHandle.Value.ToPointer() + 1;
-                    long _tar = (long)methodToReplace.MethodHandle.Value.ToPointer() + 1;
-
-                    int i = 0;
-                    do
-                    {
-                        Thread.Sleep(10); 
-                        init_inj = (long)methodToInject.MethodHandle.GetFunctionPointer().ToPointer();
-                        init_tar = (long)methodToReplace.MethodHandle.GetFunctionPointer().ToPointer();
-                        _inj = (long)methodToInject.MethodHandle.Value.ToPointer() + 1;
-                        _tar = (long)methodToReplace.MethodHandle.Value.ToPointer() + 1;
-                    }
-                    while (i++ < 20 /*&& (init_inj == _inj || init_tar == _tar)*/);
-
-                    long* inj = (long*)methodToInject.MethodHandle.Value.ToPointer() + 1;
-                    long* tar = (long*)methodToReplace.MethodHandle.Value.ToPointer() + 1;
-
-                    *tar = *inj;
+                    *(uint*)tarPtr = *(uint*)injPtr; // nur 4 Byte schreiben
                 }
             }
         }
+
+        //public static void Change(MethodInfo methodToReplace, MethodInfo methodToInject)
+        //{
+        //    unsafe
+        //    {
+        //        RuntimeHelpers.PrepareMethod(methodToReplace.MethodHandle);
+        //        RuntimeHelpers.PrepareMethod(methodToInject.MethodHandle);
+
+        //        IntPtr injPtr = methodToInject.MethodHandle.GetFunctionPointer();
+        //        IntPtr tarPtr = methodToReplace.MethodHandle.GetFunctionPointer();
+
+        //        uint oldProtect;
+        //        VirtualProtect(tarPtr, (UIntPtr)IntPtr.Size, PAGE_EXECUTE_READWRITE, out oldProtect);
+
+        //        if (IntPtr.Size == 4)
+        //        {
+        //            *(int*)tarPtr = *(int*)injPtr;
+        //        }
+        //        else
+        //        {
+        //            *(long*)tarPtr = *(long*)injPtr;
+        //        }
+
+        //        Thread.Sleep(300);
+        //        VirtualProtect(tarPtr, (UIntPtr)IntPtr.Size, oldProtect, out oldProtect);
+        //    }
+        //}
     }
 }
